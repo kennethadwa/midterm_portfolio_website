492a6aa359d9a55a89793233f512b4d32f8bdffc
kennethadwa <147363175+kennethadwa@users.noreply.github.com>
2024-09-18 20:49:49 +0800

    update

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -29,8 +29,8 @@ function Projects() {
               isBlog={false}
               title="Vector Graphic"
               description="I created a vector graphic of a potato cartoon using Krita and explored how vectors differ from raster graphics. Unlike rasters, vectors can be scaled without losing quality, which was interesting to learn during the process."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              ghLink="#"
+              demoLink="#"
             />
           </Col>
 
@@ -40,8 +40,8 @@ function Projects() {
               isBlog={false}
               title="Image Enhancing"
               description="I enhanced an image using Krita by applying an oil paint effect. I adjusted the brush size to 3 and set the smoothness to 20, transforming the normal image into a vivid, textured artwork that mimics the look of traditional oil painting."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              ghLink="#"
+              demoLink="#"
             />
           </Col>
 
@@ -51,8 +51,8 @@ function Projects() {
               isBlog={false}
               title="Movie Poster"
               description="I created my own version of the Inside Out movie poster for our activity, where we had to alter the original poster of a film. I redesigned it with a fresh perspective, using new elements and colors to bring a unique twist."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              ghLink="#"
+              demoLink="#"
             />
           </Col>
 
@@ -62,8 +62,8 @@ function Projects() {
               isBlog={false}
               title="Flip Book Sketch"
               description="I created a flipbook featuring two fish and coral reefs. The animation shows one fish biting a bait, leading to it getting caught. This sequence brings the underwater scene to life, adding a dynamic and engaging storyline to the simple drawings."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              ghLink="#"
+              demoLink="#"
             />
           </Col>
 
@@ -73,7 +73,8 @@ function Projects() {
               isBlog={false}
               title="Flip Book Animation"
               description="I animated the flipbook into a smooth animation, bringing the story of the fish and coral reefs to life. The movement of the fish biting the bait and getting caught adds a fluid and captivating element, making the underwater scene more vivid and engaging."
-              demoLink="https://editor.soumya-jit.tech/"              
+              ghLink="#"
+              demoLink="#"              
             />
           </Col>
 
@@ -83,8 +84,8 @@ function Projects() {
               isBlog={false}
               title="GIF Animation"
               description="I created a GIF using Piskel.com where I drew Goku and used over 18 frames to animate it. The multiple frames added fluid motion to the character, capturing Goku's dynamic movements and bringing the animation to life in a captivating way."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              ghLink="#"
+              demoLink="#"
             />
           </Col>
 
@@ -94,8 +95,8 @@ function Projects() {
               isBlog={false}
               title="Logo Making"
               description="I created a logo of Sagittarius from the zodiac sign and added a white circle background to resemble the moon. This design choice enhanced the logo's appearance, giving it a celestial and mystical vibe that represents the essence of the Sagittarius sign."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              ghLink="#"
+              demoLink="#"
             />
           </Col>
 
@@ -105,8 +106,8 @@ function Projects() {
               isBlog={false}
               title="Prenup Photo shoot"
               description="We did a prenup photoshoot for one of our activities with a theme inspired by the 1900s style. The vintage theme added a classic and timeless feel to the photos, capturing the elegance and charm of that era in each shot."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              ghLink="#"
+              demoLink="#"
             />
           </Col>
         </Row>
